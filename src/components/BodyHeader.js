776f16e7b05f9d5cb622ac56cf1ef138a4227720
Mokshreddy <62778004+Mokshreddy@users.noreply.github.com>
2022-09-29 15:15:04 +1000

    commit

--- src/components/BodyHeader.js
+++ src/components/BodyHeader.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react'
-import products from '../products'
+import React ,{useState, useEffect}from 'react'
+import axios from 'axios';
 
+function BodyHeader() {
 
-export default class BodyHeader extends Component {
-  render() {
-    return (
-      
-      <div className='dropdown-background'>
-       
-        <h2 className='title'> Women's Tops</h2>
-       
-       <select className='dropdown'>
-        
-            <option  >---Select Tops--- </option>
-            {
-              products.map((result)=>(<option text ={result.index}>{result.size}</option>))
-            }
-        </select>
-        
+  const [products , setProducts] = useState([]);
+
+    useEffect(function(){
+           axios.get(`https://rainy-star.surge.sh/products.json`)
+           .then(response => setProducts(response.data))
+           .then(error => console.log(error))
+    } , []);
+
+  return (
+    <div className='dropdown-background'>
+       <h2 className='title'> Women's Tops</h2>
+        <select className='dropdown'>
+        {
+          products.map((product => 
+            <option value={product.index}>{product.size}</option>
+        ))}
+       </select>  
       </div>
-    )
-  }
+  )
 }
 
+export default BodyHeader
